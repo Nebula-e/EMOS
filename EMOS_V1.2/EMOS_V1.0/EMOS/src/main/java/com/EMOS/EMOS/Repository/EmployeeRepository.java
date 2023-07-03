a64af00df13e6bd5867927e555d068de23f83bf0
Bryan Pans <125958108+Nebula-e@users.noreply.github.com>
2023-07-03 12:27:21 -0400

    Delete EMOS_V1.2/EMOS_V1.0/EMOS directory

--- EMOS_V1.2/EMOS_V1.0/EMOS/src/main/java/com/EMOS/EMOS/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EMOS.EMOS.Repository;
-
-import com.EMOS.EMOS.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
